0ef4a80e047bf5c6c847c014119c9385510063a1
andresmauricio <andresacelas@outlook.com>
2021-04-08 10:45:58 -0600

    init project Java2D

---
+++ src/cp_f3_java2d/CP_F3_Java2D.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cp_f3_java2d;
+
+/**
+ *
+ * @author PC
+ */
+public class CP_F3_Java2D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+    
+}
